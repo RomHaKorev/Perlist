3d7ac34487484675d20987e67da99080d6a9a377
Dim <romhakorev@gmail.com>
2019-10-30 22:48:26 +0100

    Add poem

---
+++ po.pl
@@ -0,0 +1,25 @@
+++$Chloe, is, my $intern for "a", "month", "now";
+
+--$Chloe
+for ("you"), I, will, goto Hell;
+
+Hell: "is each place without a padawan like you.";
+
+++$Chloe
+if I, do {want, to, train}, her;
+
+--$Chloe
+if I, do {not want, to, sadden }, her;
+
+$Chloe
+if she == my $best_intern_ever;
+
+($Because, $you) = ($are, "amazing");
+
+$Chloe = "You are the best."
+
+while $Chloe == @{[isnt, just, my $intern ]};
+
+I, will, print $Chloe,
+$just_because, " you are $you."
+
